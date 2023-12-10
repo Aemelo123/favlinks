4d2b95cfe6f5fc6a9fde68d453fc106b5c07d9b9
Aemelo123 <92276168+Aemelo123@users.noreply.github.com>
2023-12-10 12:28:00 -0500

    updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,54 @@
-function Form(){
-    return(
-        <form>
-            <label for="linkName">Link Name:</label>
-            <input type="text" id="linkName" name="linkName" value="" />
-            <br />
-            <br />
-            <label for="URL">Link URL:</label>
-            <input type="text" id="linkURL" name="linkURL" value=""/>
-            <br/>
-            <br />
-            <input type="submit" value="Submit"></input>
-        </form>
-    )
+import React, { useState } from 'react';
+
+function Form(props) {
+  const [linkName, setLinkName] = useState('');
+  const [linkURL, setLinkURL] = useState('');
+
+  const handleNameChange = (event) => {
+    setLinkName(event.target.value);
+  };
+
+  const handleURLChange = (event) => {
+    setLinkURL(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newLink = { name: linkName, URL: linkURL };
+    props.onSubmit(newLink);
+    // Clear the input fields after submission
+    setLinkName('');
+    setLinkURL('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="linkName">Link Name:</label>
+      <input
+        type="text"
+        id="linkName"
+        name="linkName"
+        value={linkName}
+        onChange={handleNameChange}
+      />
+      <br />
+      <br />
+      <label htmlFor="linkURL">Link URL:</label>
+      <input
+        type="text"
+        id="linkURL"
+        name="linkURL"
+        value={linkURL}
+        onChange={handleURLChange}
+      />
+      <br />
+      <br />
+      <input type="submit" value="Submit" />
+      <button type="button" onClick={props.resetPage}>
+        Submit Form
+      </button>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
